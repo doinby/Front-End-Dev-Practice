510acdb154ae470c98b0c65a5f7a326b33dee0df
Chinh <contact@chinhdo.co>
2020-06-27 10:16:45 +1000

    finished

--- numberguessing/app.js
+++ numberguessing/app.js
@@ -1,7 +1,7 @@
 let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
 
 // UI Elements
 const game = document.querySelector('#game');
@@ -15,6 +15,13 @@ const message = document.querySelector(".message");
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
+
 // Listen for guess
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(guessInput.value);
@@ -22,21 +29,21 @@ guessBtn.addEventListener('click', function(){
     // Validate
     if(isNaN(guess) || guess < min || guess > max){
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
-    }
-
-    // Check if won
-    if(guess === winningNum){
-        gameOver(true, `${winningNum} is correct, YOU WIN!!!`);
     } else {
-        // Wrong number
-        guessesLeft -= 1;
-
-        if (guessesLeft === 0){
-            gameOver(false, `Game Over. The correct number is ${winningNum}`);
+        // Check if won
+        if(guess === winningNum){
+            gameOver(true, `${winningNum} is correct, YOU WIN!!!`);
         } else {
-            guessInput.style.borderColor = 'red';
-            guessInput.value = '';
-            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+            // Wrong number
+            guessesLeft -= 1;
+    
+            if (guessesLeft === 0){
+                gameOver(false, `Game Over. The correct number is ${winningNum}`);
+            } else {
+                guessInput.style.borderColor = 'red';
+                guessInput.value = '';
+                setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+            }
         }
     }
 });
@@ -44,10 +51,16 @@ guessBtn.addEventListener('click', function(){
 function gameOver(won, msg){
     let color;
     won === true ? color = 'green' : color = 'red';
-
-    guessInput.disabled = true;
     guessInput.style.borderColor = color;
     setMessage(msg, color);
+
+    // Play again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className = 'play-again';
+}
+
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Set Message
