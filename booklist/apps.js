29e8f1c26dbc4c40dcd90eaa91762f64748c73f3
Chinh <contact@chinhdo.co>
2020-06-29 12:35:39 +1000

    finished 52-Add Book To List

---
+++ booklist/apps.js
@@ -0,0 +1,53 @@
+// Book constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;    
+}
+
+// UI constructor
+function UI(){}
+
+// Add book to list
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+}
+
+// Clear Fields
+UI.prototype.clearField = function(){
+    document.getElementById('title').value = '';
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+    
+    // Instantiate book constructor
+    const book = new Book(title, author, isbn);
+    
+    // Instantiate UI
+    const ui = new UI();
+    
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Clear field
+    ui.clearField();
+    
+    e.preventDefault();
+});
