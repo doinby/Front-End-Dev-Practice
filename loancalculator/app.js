a5ba8eccf64c5ffa73f74734df725d54a49898e2
Chinh <contact@chinhdo.co>
2020-07-06 09:32:02 +1000

    added UI variable

---
+++ loancalculator/app.js
@@ -0,0 +1,15 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calc results
+function calculateResults(e) {
+    console.log('Calculating...');
+    
+    // UI vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById("interest");
+    const years = document.getElementById("years");
+    const monthlyPayment = document.getElementById("monthly-payment");
+    const amount = document.getElementById("amount");
+    const amount = document.getElementById("amount");
+}
