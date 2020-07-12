16d75c2a84807b2a50a73813d5a6070efe9d5196
Chinh <contact@chinhdo.co>
2020-07-12 11:17:31 +1000

    74 - Fetch Profile Data

---
+++ githubfinder/github.js
@@ -0,0 +1,13 @@
+class Github {
+    constructor() {
+        this.client_id = "7b26bfe7f0cd877a85c5";
+        this.client_secret = "b201dec25dc5c9316920970697f8a3e21a4f5b5d";
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        return profile;
+    }
+}
