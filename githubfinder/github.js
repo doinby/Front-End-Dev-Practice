c3a306edf922de594909489aaddee7b8302bc7ff
Chinh <contact@chinhdo.co>
2020-07-13 10:22:27 +1000

    77 - Fetch & Display Repos

--- githubfinder/github.js
+++ githubfinder/github.js
@@ -2,14 +2,20 @@ class Github {
     constructor() {
         this.client_id = "7b26bfe7f0cd877a85c5";
         this.client_secret = "b201dec25dc5c9316920970697f8a3e21a4f5b5d";
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
-
+    
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+        
         return {
-          profile
+            profile,
+            repos
         }
     }
 }
--- githubfinder/ui.js
+++ githubfinder/ui.js
@@ -2,7 +2,7 @@ class UI {
     constructor() {
         this.profile = document.getElementById('profile');
     }
-
+    
     // Display profile in UI
     showProfile(user) {
         this.profile.innerHTML = `
@@ -31,7 +31,32 @@ class UI {
         <div id="repos"></div>
         `;
     }
-
+    
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+        
+        repos.forEach(function (repo) {
+            output += `
+            <div class="card card-body mb-2">
+            <div class="row">
+            <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            </div>
+            </div>
+            </div>
+            `;
+        });
+        
+        // Output repos
+        document.getElementById("repos").innerHTML = output;
+    }
+    
     // Show alert message
     showAlert(message, className) {
         // Clear remaining alerts
@@ -53,7 +78,7 @@ class UI {
             this.clearAlert();
         }, 3000);
     }
-
+    
     // Clear alert message
     clearAlert() {
         const currentAlert = document.querySelector('.alert');
@@ -61,7 +86,7 @@ class UI {
             currentAlert.remove();
         }
     }
-
+    
     // Clear profile
     clearProfile() {
         this.profile.innerHTML = '';
