5c6c642b6140174ac89e7c1babce9338bfc1467e
Chinh <contact@chinhdo.co>
2020-07-13 15:05:09 +1000

    80 - Display The Weather

--- weatherjs/app.js
+++ weatherjs/app.js
@@ -1,5 +1,6 @@
 // Initialize weather object
 const weather = new Weather('Sydney', 'AU');
+const ui = new UI();
 
 // Get weather con DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -9,6 +10,7 @@ function getWeather() {
       .getWeather()
       .then((results) => {
         console.log(results);
+        ui.paint(results);
       })
       .catch((err) => console.log(err));
 }
